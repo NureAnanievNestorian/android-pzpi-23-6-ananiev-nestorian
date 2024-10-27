83307cb075fff21944d73542cb1931200160d650
Nestorian <ananiev.nestor@ukr.net>
2024-10-27 13:24:43 +0200

    Add pract1 code

---
+++ PractTask1/android-pzpi-23-6-ananiev-nestorian-pract-task1/app/src/main/java/nure/ananiev/nestorian/pract_task1/MainActivity.java
@@ -0,0 +1,28 @@
+package nure.ananiev.nestorian.pract_task1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.text_view);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Кнопку натиснуто");
+            }
+        });
+
+    }
+}
