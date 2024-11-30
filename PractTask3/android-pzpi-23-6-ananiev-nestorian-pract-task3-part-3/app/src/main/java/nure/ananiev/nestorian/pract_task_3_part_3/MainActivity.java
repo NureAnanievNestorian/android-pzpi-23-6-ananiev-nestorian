2dca90257299b0cb1a92f50d240b950e629d56e0
NureAnanievNestorian <nestorian.ananiev@nure.ua>
2024-11-30 12:59:39 +0200

    Add pract task 3

---
+++ PractTask3/android-pzpi-23-6-ananiev-nestorian-pract-task3-part-3/app/src/main/java/nure/ananiev/nestorian/pract_task_3_part_3/MainActivity.java
@@ -0,0 +1,41 @@
+package nure.ananiev.nestorian.pract_task_3_part_3;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        MyAdapter adapter = new MyAdapter(getDataSet());
+        recyclerView.setAdapter(adapter);
+    }
+
+    private String[] getDataSet() {
+        String[] dataSet = new String[100];
+        for (int i = 0; i < dataSet.length; i++) {
+            dataSet[i] = "String " + i;
+        }
+        return dataSet;
+    }
+}
